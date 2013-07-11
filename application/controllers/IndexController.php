0c30d0de47159e8a244e7f2c993db24e99d8e138
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-07-11 10:38:22 -0700

    Miscellaneos cleanup

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,35 +10,35 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        
+
         // Instantiate the FirstFriday object
         $ff = new PHX2600_FirstFriday;
-        
+
         // Get the First Friday timestamp
         $this->view->ff = $ff->firstFriday(false);
-        
+
         // Determine if there's a meeting this week
         if (date('Ymd', time()) == date('Ymd', $this->view->ff)) {
-            
+
             // There's a meeting tonight
             $this->view->meetingTonight = true;
-            
+
         } elseif ($this->view->ff - time() <= 432000) {
-            
+
             // There's a meeting this Friday
             $this->view->meetingThisFriday = true;
-            
+
         }
-        
+
         // Instantiate the Zend_Cache from application.ini
         $cache = $this->getFrontController()
             ->getParam('bootstrap')
             ->getPluginResource('cachemanager')
             ->getCacheManager()->getCache('default');
-        
+
         // Load forum feed from cache if applicable else fetch it and cache it
         if ( ($this->view->forumFeed = $cache->load('forumFeed')) === false ) {
-            
+
             try {
                 // Instantiate forum Atom object
                 $this->view->forumFeed = new Zend_Feed_Atom('https://groups.google.com/forum/feed/phx2600/topics/atom.xml?num=15');
@@ -46,12 +46,14 @@ class IndexController extends Zend_Controller_Action
                 // Set feed data to false on error
                 $this->view->forumFeed = false;
             }
-            
+
+            // var_dump($this->view->forumFeed); die(); // Debugging
+
             // Cache the feed
             $cache->save($this->view->forumFeed, 'forumFeed');
 
         }
-        
+
     }
 
 }
